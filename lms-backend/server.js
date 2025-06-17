aae3f4ee02ed4a61d0c272b1d7c005d995c960f5
rakshitDarji007 <rakshit.darji007@gmail.com>
2025-06-17 15:08:52 -0500

    server.js

--- lms-backend/server.js
+++ lms-backend/server.js
@@ -1,1 +1,13 @@
 require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.get('/', (req, res) => {
+    res.send('API RUNNING, YIPPEE(LMS)')
+})
+
+app.listen(PORT, () => {
+    console.log('Server running on port ${PORT}')
+})
