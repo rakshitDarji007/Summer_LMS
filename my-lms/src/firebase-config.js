6dd5ef6a8777c60f829cc4b544c656530a36f226
rakshitDarji007 <rakshit.darji007@gmail.com>
2025-06-29 07:00:23 -0500

    intial fiirebase setup

---
+++ my-lms/src/firebase-config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore} from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDmBBIkYa6fEcmDGEt-Fd4IVdZs8EBCQoE",
+    authDomain: "summerlms.firebaseapp.com",
+    projectId: "summerlms",
+    storageBucket: "summerlms.firebasestorage.app",
+    messageSenderId: "303082571702",
+    appId: "1:303082571702:web:e97ce622ef823c948ee317",
+    measurementId: "G-XL7RSNTD2W"
+};
