bed06251f41a8aa53094fc033d16f7eec9f368e4
rakshitDarji007 <rakshit.darji007@gmail.com>
2025-06-29 08:43:53 -0500

    did the user authentication and the user states

--- my-lms/src/App.jsx
+++ my-lms/src/App.jsx
@@ -5,3 +5,47 @@ import { doc, getDoc } from 'firebase/firestore';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
 import Dashboard from './components/Dashboard';
+
+function App(){
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  useEffect(()=> {
+    const unsubscribe = onAuthStateChanged(auth, async (currentUser) => {
+      if(currentUser) {
+        const userDocRef = doc(db, 'users', currentUser.uid);
+        const userDoc = await getDoc(userDocRef);
+        if(userDoc.exists()){
+          setUser({ ...currentUser, role: userDoc.data().role});
+        } else {
+          setUser(currentUser);
+        }
+      } else {
+        setUser(null);
+      }
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="app-container">
+      {!user ? (
+        <>
+          <SignUp />
+          <hr />
+          <Login />
+        </>
+      ) : (
+        <Dashboard user={user} />
+      )}
+      </div>
+  );
+}
+
+export default App;
